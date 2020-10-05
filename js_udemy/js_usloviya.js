b0de60d042b1aa3c6ce9fdde4b3bfdce139b7b48
Aleksandr <lestad01@mail.ru>
2020-10-05 14:56:09 +0500

    js_udemy_uskiviya/05.10.20

---
+++ js_udemy/js_usloviya.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//if (4 == 8) {
+    //console.log('ok!');
+//} else {
+    //console.log('Error!');
+//}
+
+
+
+//if (num < 49) {
+    //console.log('Error!');
+//} else if (num > 100) {
+   // console.log('too much!');
+//} else {
+   // console.log('Good!');
+//}
+
+
+//const num = 50;
+
+//(num == 50) ? console.log('Ok!'): console.log('Error!');
+
+const nam = '48';
+
+switch(nam) {
+    case '49': 
+        console.log('Неверно!');
+        break;
+    case '228':
+        console.log('Неверно!');
+        break;
+    default:
+        console.log('Не судьба!');
+        break;
+    case '48':
+        console.log('Bingo!');
+        break;
+}
