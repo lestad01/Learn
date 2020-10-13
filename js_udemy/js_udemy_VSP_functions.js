a3fa4b1d7a5e9c0d8eaa7279decb043ae5ddde9f
Aleksandr <lestad01@mail.ru>
2020-10-13 16:58:05 +0500

    13.10.2020

---
+++ js_udemy/js_udemy_VSP_functions.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = "stoperes";
+const arr = [2, 5, 9];
+console.log(arr.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+
+const logg = "Hello WoRLD";
+
+console.log(logg.slice(-6, -1));
+
+console.log(logg.substring(6, 11)); /*не поддерживает отрицательные значения*/
+
+console.log(logg.substr(6, 1));
+
+const numm = 12.2;
+console.log(Math.round(numm));
+
+const tes = "12.2228px";
+console.log(parseInt(tes));
+console.log(parseFloat(tes));
+
