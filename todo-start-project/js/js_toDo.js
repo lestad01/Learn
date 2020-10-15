ad86c7b85642ac98281a509fda31fa0add725c12
Aleksandr <lestad01@mail.ru>
2020-10-15 17:37:28 +0500

    15.10.20

--- todo-start-project/js/js_toDo.js
+++ todo-start-project/js/js_toDo.js
@@ -1,7 +1,10 @@
 const forms = document.querySelector('#newTaskForm');
 const texts = document.querySelector('#addNewTask');
 const tasksList = document.querySelector('#list-group');
-const emptyListItem = document.querySelector('#empty-list-item');
+
+
+//Загружаем данные
+loadData();
 
 // 1. Добавление новой задачи
 // Отслеживаем отправку формы
@@ -18,8 +21,8 @@ forms.addEventListener('submit', function (evr) {
     const taskHTML = `<li class="list-group-item d-flex justify-content-between">
                         <span contenteditable="true" class="task-title">${tasksText}</span>
                         <div>
-                            <button type="button" data-action="ready" class="btn btn-light align-self-end">Готово</button>
-                            <button type="button" data-action="delete-task" class="btn btn-light align-self-end">Удалить</button>
+                <button type="button" data-action="ready" class="btn btn-light align-self-end">Готово</button>
+                <button type="button" data-action="delete-task" class="btn btn-light align-self-end">Удалить</button>
                         </div>
                     </li>`;
 
@@ -34,14 +37,81 @@ forms.addEventListener('submit', function (evr) {
 
     // Возвращаем фокус на поле ввода после добавления задачи
     texts.focus();
+    
+    //Сохранить данные
+    saveData();
+});
+
+deleteListItem();
+
+
+//2. КНОПКИ "Готово" и "Удалить"
+// Прослушиваем клик внутри всего списка с задачами
+
+tasksList.addEventListener('click', function(event) {
+
+    //Проверяем,что клик произошел по кнопке "Удалить"
+    if (event.target.getAttribute('data-action') == 'delete-task'){
+    
+        //Находим родительски тег <li> или .list-group-item и удаляем его
+        event.target.closest('.list-group-item').remove();
+
+         // Скрывает или показывает запись о том что список дел чист
+        deleteListItem();
+
+        //Сохранить данные
+        saveData();
+
+        //проверяем, что клик произошел по кнопке "Готово"
+    }else if (event.target.getAttribute('data-action') == 'ready') {
+
+        //Нашли родительский элемент Li
+        const parrentElement = event.target.closest('.list-group-item');
+
+        //В родительском элементе через пер. нашли спан и присвоили ему класс
+        parrentElement.querySelector('span.task-title').classList.add('task-title-done');
+
+        parrentElement.querySelector('span.task-title').setAttribute('contenteditable','false');
+
+        //Перемещаем запись в конец списка
+        tasksList.insertAdjacentElement('beforeend', parrentElement);
+
+        //Удаляем кнопку "Готово"
+        parrentElement.querySelector('button[data-action="ready"]').remove();
+
+        //Сохранить данные
+        saveData();
+    }
 });
 
 function deleteListItem() {
     if (tasksList.children.length > 1) {
-        emptyListItem.style.display = "none";
+        document.querySelector('#empty-list-item').emptyListItem.style.display = "none";
     }else{
-        emptyListItem.style.display = "block";
+        document.querySelector('#empty-list-item').emptyListItem.style.display = "block";
     }
 }
 
-deleteListItem();
+function saveData() {
+    localStorage.setItem('todoList',tasksList.innerHTML);
+}
+
+function loadData() {
+    if (localStorage.getItem('todoList')) {
+    tasksList.innerHTML = localStorage.getItem('todoList');
+    }
+}
+
+
+
+
+
+
+
+
+// //Сохраняем данные в localstorage
+// localStorage.setItem('user', 'PabloEskobar');
+
+// //Получаем данныеиз localStorage
+// localStorage.getItem('user');
+
