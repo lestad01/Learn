50e00b473086b7e5b47dba6b726cccfa9ce5a770
Aleksandr <Lestad01@mail.ru>
2020-10-21 23:15:40 +0300

    js_udemy_21.10_20

--- js_udemy/js_udemy_objects.js
+++ js_udemy/js_udemy_objects.js
@@ -17,43 +17,28 @@ options.makeTest();
 
 
 const{border,background} = options.colors; //деструктуризация (вытащили 2 ключа в отдельную переменную)
-console.log(border);
+console.log(background);
 
-//console.log(Object.keys(options).length);
-
-
-
-// const prot = {
-//     user: 'ka4',
-//     password: 228,
-//     description: '',
-//     birthday: {
-//         day: 1,
-//         month: 7,
-//         year: 1995
-//     }
-   
-// };
-
-// console.log(Object.keys(prot));
+console.log(Object.keys(options).length);
 
 
-// console.log(options.name);
-// delete options.width;
 
-// console.log(options);
+console.log(options.name);
+delete options.width;
+
+console.log(options);
 
 
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         }
-//     }else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter++;
-//     }
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    }else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
     
-// }
-// console.log(counter);
+}
+console.log(counter);
--- js_udemy/js_udemy_ssilka.js
+++ js_udemy/js_udemy_ssilka.js
@@ -1,17 +1,17 @@
 
 "use strict";
 
-let a = 5,
-    b = a;
+// let a = 5,
+//     b = a;
 
-b = b + 5;
-console.log(b);
-console.log(a);
+// b = b + 5;
+// console.log(b);
+// console.log(a);
 
-const obj = {
-    a: 5,
-    b: 1
-};
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
 // const copy = obj; //ссылку (передача по ссылке)
 
@@ -20,27 +20,101 @@ const obj = {
 // console.log(copy);
 // console.log(obj);
 
-function copy(mainObj) {
-    let objCopy = {};
+// function copy(mainObj) {
+//     let objCopy = {};
     
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
 
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.b = 10;
+// console.log(newNumbers);
+// console.log(numbers);
+
+
+const add = {
+    d: 17,
+    w: 9
 };
 
-const newNumbers = copy(numbers);
+//console.log(Object.assign(numbers, add)); // - поверхностная копия объекта
 
-newNumbers.a = 10;
-console.log(newNumbers);
-console.log(numbers);
+const clone = Object.assign({}, add);
+
+clone.d = 22;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'Hello,broucher!';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a,b,c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const nam = [2,5,7];
+
+log(...nam);
+//
+
+const arrray = ["a", "b"];
+
+const newAarray = [...arrray];
+console.log(newAarray);
+
+//
+
+// function stay(From) {
+//     let newConst = {}
+
+//     let i;
+//     for (i in From) {
+//         newConst[i] = From[i]; 
+//     }
+//     return newConst;
+// }
+
+// const num = {
+//     f: 11,
+//     l: 4,
+//     p: {
+//         h:22,
+//         K:31
+//     }
+// };
+
+// const NewNum = stay(num);
+// num.l = 16;
+
+// console.log(num);
+// console.log(NewNum);  Поверхностные копии объектов.
+
+
+
