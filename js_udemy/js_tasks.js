cd330e7df5b382cc0fc785fff847cb7d51190329
Aleksandr <lestad01@mail.ru>
2020-11-03 17:42:02 +0500

    js_tasks

---
+++ js_udemy/js_tasks.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let x = 5;
+// alert(x++); //5
+
+// [] + false - null + true
+// console.log([] + false - null + true); // "NaN"
+
+// let y = 1;
+// let x = y = 2;
+// alert(x); // 2
+
+
+// [] + 1 + 2; // "12"
+
+
+// alert("1"[0]) //1
+
+// console.log(2 && 1 && null && 0 && undefined); //null
+
+
+// console.log(!!(1 && 2 ) === (1 && 2)); //false
+
+// alert( null || 2 && 3 || 4 ); //3
+
+// const a = [1,2,3];
+// const b = [1,2,3];
+// console.log(a == b); //false
+
+alert(+"Infinity"); // number
+
+
+console.log("Ёжик" < "яблоко");
