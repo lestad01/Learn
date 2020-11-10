192d3a1c9f89992a63e16318f2cc164ad6e6e475
Aleksandr <lestad01@mail.ru>
2020-11-10 18:23:09 +0500

    10/11/20

--- js_figma/js_figma_audio.js
+++ js_figma/js_figma_audio.js
@@ -4,6 +4,8 @@ let audio = document.getElementById("audio");
 let time = document.querySelector(".time");
 let play = document.querySelector(".play");
 let pause = document.querySelector(".pause");
+let pauseImg = document.querySelector(".img_pause");
+let playImg = document.querySelector(".img_play");
 
 let playlist = [
     'track1.mp3',
@@ -15,29 +17,20 @@ window.onload = function () {
     track = 0;
 };
 
-play.addEventListener("click", function() {
+pause.addEventListener("click", function() {
     audio.currentTime = 0;
     audio.play();
-      audioPlay = setInterval(function() {
+    pause.classList.add('play');
+    pauseImg.style.display = "none";
+    playImg.style.display = "block";
+     let audioPlay = setInterval(function() {
         let audioTime = Math.round(audio.currentTime);
         let audioLength = Math.round(audio.duration);
         time.style.width = (audioTime * 100) / audioLength + '%';
+        audioPlay();
     });
+
 });
 
 
-pause.addEventListener("click", function() {
-    audio.pause();
-    clearInterval(audioPlay);
-});
-
- play.onclick = (event) => {
-            console.log(event.target);
-            event.target.classList.add('pause');
-        };
     
-
-
-
-/*header.classList.add("header_scroll");
- header.classList.remove("header_scroll")*/
