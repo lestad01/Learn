baf7c14177b6daae01187f7f6379799627113461
Aleksandr <Lestad01@mail.ru>
2020-11-12 02:13:38 +0300

    12/11/2020

--- js_figma/js_figma_audio.js
+++ js_figma/js_figma_audio.js
@@ -34,59 +34,16 @@ playImg.style.display = "none";
 //     });
 });
 
-$('.muted').click(function () {
-    audio.muted = !audio.muted;
-    return false;
-});let volumeDrag = false;
-$('.volume').on('mousedown', function (e) {
-    volumeDrag = true;
-    audio.muted = false;
-    $('.sound').removeClass('muted');
-    updateVolume(e.pageX);
+//slider 
+function outputUpdate(vol) {
+    const output = document.getElementById('volume');
+    output.value = vol;
+    output.style.center = vol - 1 + 'px';
+}
+
+// jQuery громкость 
+$("#fader").mousemove(function (){
+    audio.volume = parseFloat(this.value / 100);
 });
-$(document).on('mouseup', function (e) {
-    if (volumeDrag) {
-        volumeDrag = false;
-        updateVolume(e.pageX);
-    }
-});
-$(document).on('mousemove', function (e) {
-    if (volumeDrag) {
-        updateVolume(e.pageX);
-    }
-});
-let updateVolume = function (x, vol) {
-    let volume = $('.volume');
-    let percentage;
-    //if only volume have specificed
-    //then direct update volume
-    if (vol) {
-        percentage = vol * 100;
-    } else {
-        let position = x - volume.offset().left;
-        percentage = 100 * position / volume.width();
-    }
-
-    if (percentage > 100) {
-        percentage = 100;
-    }
-    if (percentage < 0) {
-        percentage = 0;
-    }
-
-    //update volume bar and video volume
-    $('.volumeBar').css('width', percentage + '%');
-    audio.volume = percentage / 100;
-
-    //change sound icon based on volume
-    if (audio.volume == 0) {
-        $('.sound').removeClass('sound2').addClass('muted');
-    } else if (audio.volume > 0.5) {
-        $('.sound').removeClass('muted').addClass('sound2');
-    } else {
-        $('.sound').removeClass('muted').removeClass('sound2');
-    }
-};
-
 
     
