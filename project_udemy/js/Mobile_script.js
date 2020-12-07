fedb22f8a00c25412df0d158663a4b796180e711
Aleksandr <lestad01@mail.ru>
2020-12-07 19:19:56 +0500

    07.12.20

--- project_udemy/js/Mobile_script.js
+++ project_udemy/js/Mobile_script.js
@@ -10,9 +10,28 @@
 window.addEventListener("DOMContentLoaded", ()=> {
 	const box = document.querySelector('.box');
 
-	box.addEventListener('touchstart', (e) => {
+	box.addEventListener('touchmove', (e) => {
 		e.preventDefault();
 
-		console.log('Start');
+		console.log(e.targetTouches[0].pageX);
+		// console.log('Start');
+		// console.log(e.touches);
+		// console.log(e.targetTouches);
+		// console.log(e.changedTouches);
 	});
+	// box.addEventListener('touchmove', (e) => {
+	// 	e.preventDefault();
+
+	// 	console.log('Move');
+	// });
+	// box.addEventListener('touchend', (e) => {
+	// 	e.preventDefault();
+
+	// 	console.log('End');
+	// });
 });
+
+
+//touches 
+// targetTouches
+//changedTouches
